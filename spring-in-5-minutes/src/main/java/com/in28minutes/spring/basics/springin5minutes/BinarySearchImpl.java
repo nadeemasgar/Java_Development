2769f5109cb2b5f2003daf25c83b45768cace134
Nadeem <nadeemasgar18@gmail.com>
2023-08-05 18:33:33 +0530

    tightly coupled code

---
+++ spring-in-5-minutes/src/main/java/com/in28minutes/spring/basics/springin5minutes/BinarySearchImpl.java
@@ -0,0 +1,14 @@
+package com.in28minutes.spring.basics.springin5minutes;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearch) {
+		// Implementing Sorting Logic -> Bubble Sort, Quick Sort
+		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		// Search the array
+		// Return the result
+		return 3;
+	}
+	
+}
---
+++ spring-in-5-minutes/src/main/java/com/in28minutes/spring/basics/springin5minutes/BubbleSortAlgorithm.java
@@ -0,0 +1,9 @@
+package com.in28minutes.spring.basics.springin5minutes;
+
+public class BubbleSortAlgorithm {
+	
+	public int[] sort(int[] numbers) {
+		// Logic for Bubble Sort
+		return numbers;
+	}
+}
